3a5f3f3bae94af45f5c8a0728a8df499a6940500
micellssgui <michellss.gui@gmail.com>
2022-06-26 17:32:28 -0300

    Trabalhando no scopo/layout do app curriculo

---
+++ curriculo/static/js/script.js
@@ -0,0 +1,28 @@
+function abreAcademico(){
+    academicoOpen = document.getElementById('academico').style.display = 'block'
+    /*btn = document.getElementById('btn-academico').style.display = 'block'*/
+}
+
+function abreSoft(){
+    softOpen = document.getElementById('soft').style.display = 'block'
+    /*btn = document.getElementById('btn-soft').style.display = 'block'*/
+}
+
+function abreHard(){
+    hardOpen = document.getElementById('hard').style.display = 'block'
+    /*btn = document.getElementById('btn-hard').style.display = 'block'*/
+}
+
+
+
+function fechaAcademico(){
+    academicoOpen = document.getElementById('academico').style.display = 'none'
+}
+
+function fechaSoft(){
+    softOpen = document.getElementById('soft').style.display = 'none'
+}
+
+function fechaHard(){
+    hardOpen = document.getElementById('hard').style.display = 'none'
+}
